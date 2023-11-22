065e6a0f21143b731ad1eb512a8654a49b10d76b
perun <jelesije@hotmail.com>
2023-11-22 17:46:41 +0100

    update

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "resource" {
-value       = azurerm_network_ddos_protection_plan.this
-description = "The ddos protection plan resource."
+  value       = azurerm_network_ddos_protection_plan.this
+  description = "The ddos protection plan resource."
 }
