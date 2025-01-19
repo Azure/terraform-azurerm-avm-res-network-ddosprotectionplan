67cf81c38639fa6f54132673e0e4b18cf3d7ab18
Jared Holgate <jaredholgate@microsoft.com>
2025-01-19 11:44:53 +0000

    Fix required providers

--- examples/default-azurerm-v3/main.tf
+++ examples/default-azurerm-v3/main.tf
@@ -35,6 +35,7 @@ terraform {
     }
   }
 }
+
 provider "azurerm" {
   features {}
 }
--- terraform.tf
+++ terraform.tf
@@ -3,11 +3,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.71.0, < 5.0"
+      version = ">= 3.116, < 5.0"
+    }
+    modtm = {
+      source  = "azure/modtm"
+      version = "~> 0.3"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.5.0"
+      version = "~> 3.5"
     }
   }
 }
